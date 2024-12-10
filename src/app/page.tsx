3c00137e235806e98bee2c3cae2cf4880a8fd45b
LeeDongTak <sunny5796@naver.com>
2024-12-10 22:09:37 +0900

    fix: 안쓰는 코드 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,5 @@
 import MainSection from "@/components/main/main-section/main-section";
 import SubSection from "@/components/main/sub-section/sub-section";
-import Image from "next/image";
 
 export default function Home() {
   return (
