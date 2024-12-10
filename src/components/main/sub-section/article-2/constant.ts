4faaa045f343d08d8eecedaed5707d58e2051679
LeeDongTak <sunny5796@naver.com>
2024-12-10 20:57:30 +0900

    design: 서브 아테클 2완료

---
+++ src/components/main/sub-section/article-2/constant.ts
@@ -0,0 +1,69 @@
+export const SUB_ARTICLE_TOW_LIST = [
+  {
+    id: 0,
+    content: "팬이 만든 거래 플랫폼!",
+  },
+  {
+    id: 1,
+    content: "물품 등록 한 번으로 스트레스 없이 거래 완료!",
+  },
+  {
+    id: 2,
+    content: "100% 실명 인증된 팬들과 거래",
+  },
+  {
+    id: 3,
+    content: "강력한 사기 방지 시스템!",
+  },
+];
+
+export const SUB_ARTICLE_TOW_FILTER_LIST_ONE = [
+  {
+    id: 0,
+    content: "드라마",
+    isDone: false,
+  },
+  {
+    id: 1,
+    content: "영화",
+    isDone: false,
+  },
+  {
+    id: 2,
+    content: "K-POP",
+    isDone: true,
+  },
+  {
+    id: 3,
+    content: "웹툰",
+    isDone: false,
+  },
+  {
+    id: 4,
+    content: "애니",
+    isDone: false,
+  },
+];
+
+export const SUB_ARTICLE_TOW_FILTER_LIST_TWO = [
+  {
+    id: 0,
+    content: "BTS",
+    isDone: false,
+  },
+  {
+    id: 1,
+    content: "뉴진스",
+    isDone: false,
+  },
+  {
+    id: 2,
+    content: "ITZY",
+    isDone: false,
+  },
+  {
+    id: 3,
+    content: "SVT",
+    isDone: true,
+  },
+];
---
+++ src/components/main/sub-section/article-2/sub-article-2.tsx
@@ -0,0 +1,82 @@
+import React from "react";
+import Check from "../../../../../public/icons/check.svg";
+import ArrowBottom from "../../../../../public/icons/arrowBottom.svg";
+import Image from "next/image";
+import {
+  SUB_ARTICLE_TOW_FILTER_LIST_ONE,
+  SUB_ARTICLE_TOW_FILTER_LIST_TWO,
+  SUB_ARTICLE_TOW_LIST,
+} from "./constant";
+import clsx from "clsx";
+
+const SubArticle2 = () => {
+  return (
+    <article className="flex justify-between items-start w-[1200px] h-auto mx-auto pt-[247px] pb-[10rem]">
+      <div className="flex flex-col justify-center items-center gap-[3.5rem] flex-1">
+        <h2 className="text-[4rem] leading-[5.5rem] font-[700]">
+          굿즈거래, <span className="text-[#7986EF]">덕메이트</span>로
+          <br /> 더이상 스트레스 NO!
+        </h2>
+        <ul className="flex flex-col justify-start gap-[4rem] shadow-[0_0_6px_rgba(202,208,255,1)] py-[9.6rem] px-[4.9rem] rounded-[1rem] border border-solid border-[rgba(202,208,255,0.3)] bg-[rgba(255,255,255,0.3)]">
+          {SUB_ARTICLE_TOW_LIST.map(({ id, content }) => {
+            return (
+              <li key={id} className="flex items-center gap-[1.5rem]">
+                <Image src={Check} alt="체크 아이콘" />
+                <p className="text-[2rem] leading-[2rem] font-[500]">
+                  {content}
+                </p>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+      {/* //////////// */}
+      <div className="flex flex-col justify-center items-center gap-[3.5rem] flex-1">
+        <h2 className="text-[4rem] leading-[5.5rem] font-[700]">
+          내가 <span className="text-[#7986EF]">원하는 소통</span>만!
+          <br /> AI로 완벽한 필터링
+        </h2>
+        <div className="flex flex-col justify-start items-center gap-[2.4rem] shadow-[0_0_6px_rgba(202,208,255,1)] py-[5.8rem] px-[4.9rem] rounded-[1rem] border border-solid border-[rgba(202,208,255,0.3)] bg-[rgba(255,255,255,0.3)]">
+          <ul className="flex justify-center gap-[1.2rem]">
+            {SUB_ARTICLE_TOW_FILTER_LIST_ONE.map(({ id, content, isDone }) => {
+              return (
+                <li
+                  key={id}
+                  className={clsx(
+                    "flex items-center text-[1.6rem] leading-[1.6rem] font-[500]] px-[1.4rem] py-[0.8rem] rounded-full",
+                    isDone
+                      ? " bg-[#79b6ef] text-[#fff]"
+                      : "bg-[#f5f5f5] text-[#3E3E3E]"
+                  )}
+                >
+                  {content}
+                </li>
+              );
+            })}
+          </ul>
+          <Image src={ArrowBottom} alt="화살표이미지" />
+          <ul className="flex justify-center gap-[1.2rem]">
+            {SUB_ARTICLE_TOW_FILTER_LIST_TWO.map(({ id, content, isDone }) => {
+              return (
+                <li
+                  key={id}
+                  className={clsx(
+                    "flex items-center text-[1.6rem] leading-[1.6rem] font-[500]] px-[1.4rem] py-[0.8rem] rounded-full",
+                    isDone
+                      ? " bg-[#79b6ef] text-[#fff]"
+                      : "bg-[#f5f5f5] text-[#3E3E3E]"
+                  )}
+                >
+                  {content}
+                </li>
+              );
+            })}
+          </ul>
+          <div className="w-[38.4rem] h-[11rem] rounded-[1rem] bg-[#eaeaea]"></div>
+        </div>
+      </div>
+    </article>
+  );
+};
+
+export default SubArticle2;
